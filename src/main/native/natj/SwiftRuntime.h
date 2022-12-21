f45100ee72cbc115321c9df443e61000eb97895c
Berstanio <berstaniodev@gmail.com>
2022-12-21 13:44:19 +0100

    Add spotlessApply for swift files

--- src/main/native/natj/SwiftRuntime.h
+++ src/main/native/natj/SwiftRuntime.h
@@ -3,6 +3,6 @@
 extern jobject getSwiftRuntime();
 
 extern "C" {
-    JNIEXPORT void JNICALL Java_org_moe_natj_swift_SwiftRuntime_initialize(JNIEnv* env, jclass clazz, jobject instance);
-    JNIEXPORT void JNICALL Java_org_moe_natj_swift_SwiftRuntime_registerClass(JNIEnv* env, jclass clazz, jclass type);
+JNIEXPORT void JNICALL Java_org_moe_natj_swift_SwiftRuntime_initialize(JNIEnv* env, jclass clazz, jobject instance);
+JNIEXPORT void JNICALL Java_org_moe_natj_swift_SwiftRuntime_registerClass(JNIEnv* env, jclass clazz, jclass type);
 }
