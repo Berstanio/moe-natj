ab7704d0c43a4123a14e5e740e4aeadd5aec8e19
Berstanio <berstaniodev@gmail.com>
2022-12-21 13:44:32 +0100

    Small refactoring

--- src/main/java/org/moe/natj/swift/map/SwiftObjectMapper.java
+++ src/main/java/org/moe/natj/swift/map/SwiftObjectMapper.java
@@ -19,27 +19,38 @@ public class SwiftObjectMapper implements Mapper {
         return ((NativeObject) instance).getPeer().getPeer();
     }
 
+    private boolean isStruct(Class<?> aClass) {
+        return StructObject.class.isAssignableFrom(aClass);
+    }
+
+    private Class<?> classToInstantiate(Pointer pointer, Class<?> type) {
+        long instance = pointer.getPeer();
+        boolean isProtocolReturn = type.isAnnotationPresent(SwiftProtocol.class);
+        Class<?> classForInstance = null;
+        if (isProtocolReturn) {
+            // Look at definition of existential container, to see where the 24 comes from
+            // 3 * value, 1 * metadata, 1 * pwt
+            long metadataPointer = SwiftRuntime.dereferencePeer(instance + 24);
+            classForInstance = SwiftRuntime.getClassForMetadataPointer(metadataPointer);
+            if (classForInstance == null) throw new RuntimeException("No binding found for protocol, currently unsupported");
+            instance = SwiftRuntime.dereferencePeer(instance);
+            // TODO: 08.12.22 For structs the EC doesn't directly point to the struct. So we need a small offset
+            if (isStruct(classForInstance)) instance += 16;
+            pointer.setPeer(instance);
+        } else if (!isStruct(type)) {
+            classForInstance = SwiftRuntime.getClassForPeer(instance);
+        }
+        pointer.setPeer(instance);
+        return classForInstance;
+    }
+
     @Override
     public Object toJava(long instance, NatJ.JavaObjectConstructionInfo info) {
         Pointer pointer = new Pointer(instance);
         try {
             Constructor<?> constructor;
             synchronized (info) {
-                Class<?> classForInstance = null;
-                boolean isProtocolReturn = info.type.isAnnotationPresent(SwiftProtocol.class);
-                if (isProtocolReturn) {
-                    // Look at definition of existential container, to see where the 24 comes from
-                    // 3 * value, 1 * metadata, 1 * pwt
-                    long metadataPointer = SwiftRuntime.dereferencePeer(instance + 24);
-                    classForInstance = SwiftRuntime.getClassForMetadataPointer(metadataPointer);
-                    if (classForInstance == null) throw new RuntimeException("No binding found for protocol, currently unsupported");
-                    instance = SwiftRuntime.dereferencePeer(instance);
-                    // TODO: 08.12.22 For structs the EC doesn't directly point to the struct. So we need a small offset
-                    if (StructObject.class.isAssignableFrom(classForInstance)) instance += 16;
-                    pointer.setPeer(instance);
-                } else if (!StructObject.class.isAssignableFrom(info.type)) {
-                    classForInstance = SwiftRuntime.getClassForPeer(instance);
-                }
+                Class<?> classForInstance = classToInstantiate(pointer, info.type);
                 if (classForInstance != null) {
                     // TODO: 07.12.22 Only with getConstructor? To improve inheritance?
                     constructor = classForInstance.getDeclaredConstructor(Pointer.class);
